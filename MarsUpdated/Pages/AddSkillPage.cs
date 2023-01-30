1d80afa406afe4bcb6028c398db9ea8edee40800
mohanaguntakatta <moni.snigdha@gmail.com>
2023-01-30 18:26:33 +1100

    Final Changes

--- MarsUpdated/Pages/AddSkillPage.cs
+++ MarsUpdated/Pages/AddSkillPage.cs
@@ -3,6 +3,7 @@ namespace MarsUpdated;
 
 public class AddSkillPage : CommonDriver
 {
+    // Wait  inside the constructor
 
     public WebDriverWait wait;
     public AddSkillPage()
@@ -10,6 +11,7 @@ public class AddSkillPage : CommonDriver
         wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
     }
 
+    // Implemented the POM pattern
     public IWebElement SkillButton => driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[1]/a[2]"));
     public IWebElement AddnewButton => driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/thead/tr/th[3]/div"));
     public IWebElement AddskillTextbox => driver.FindElement(By.Name("name"));
